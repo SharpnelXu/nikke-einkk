937903c88c228d24c53297b33aedcd8a5c1b62c7
Yome <695312637@qq.com>
2025-04-15 23:56:07 -0700

    Parse CharacterTable

---
+++ lib/model/nikke.dart
@@ -0,0 +1,255 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'nikke.g.dart';
+
+// sample data
+// {
+// "id": 352004,
+// "name_localkey": "Locale_Character:520_name",
+// "description_localkey": "Locale_Character:c520_description",
+// "resource_id": 520,
+// "additional_skins": [],
+// "name_code": 5135,
+// "order": 10111,
+// "original_rare": "SSR",
+// "grade_core_id": 4,
+// "grow_grade": 352005,
+// "stat_enhance_id": 5102,
+// "class": "Attacker",
+// "element_id": [
+// 200001
+// ],
+// "critical_ratio": 1500,
+// "critical_damage": 15000,
+// "shot_id": 1052001,
+// "bonusrange_min": 45,
+// "bonusrange_max": 100,
+// "use_burst_skill": "Step3",
+// "change_burst_step": "StepFull",
+// "burst_apply_delay": 1,
+// "burst_duration": 1000,
+// "ulti_skill_id": 1520301,
+// "skill1_id": 2520101,
+// "skill1_table": "StateEffect",
+// "skill2_id": 2520201,
+// "skill2_table": "StateEffect",
+// "eff_category_type": "Walk",
+// "eff_category_value": 0,
+// "category_type_1": "None",
+// "category_type_2": "None",
+// "category_type_3": "None",
+// "corporation": "TETRA",
+// "cv_localkey": "Locale_Character:520_cv",
+// "squad": "CookingOil",
+// "piece_id": 5100520,
+// "is_visible": true,
+// "prism_is_active": false,
+// "is_detail_close": false
+// }
+
+@JsonSerializable()
+class NikkeCharacterData {
+  // format "corporation + resourceId + gradeCoreId", e.g. 235004, series = 2, resourceId = 350, gradeCoreId = 04
+  final int id;
+  @JsonKey(name: 'name_localkey')
+  final String nameLocalkey;
+  @JsonKey(name: 'description_localkey')
+  final String descriptionLocalkey;
+  @JsonKey(name: 'resource_id')
+  final int resourceId;
+  // values observed in list are "acc", "bg". Yet to know what this refers to, but probably doesn't matter
+  @JsonKey(name: 'additional_skins')
+  final List<String> additionalSkins;
+  // doesn't know what this means
+  @JsonKey(name: 'name_code')
+  final int nameCode;
+  // doesn't know what this means
+  final int order;
+  // valid values are "SSR", "SR", "R"
+  @JsonKey(name: 'original_rare')
+  final Rarity originalRare;
+  // valid values are [1, 11], use `limitBreak` for more readable format
+  @JsonKey(name: 'grade_core_id')
+  final int gradeCoreId;
+  // denote if there's next grade, 0 means no next grade
+  @JsonKey(name: 'grow_grade')
+  final int growGrade;
+  // CharacterStatEnhanceTable
+  @JsonKey(name: 'stat_enhance_id')
+  final int statEnhanceId;
+  // "Attacker", "Defender", "Supporter"
+  @JsonKey(name: 'class')
+  final NikkeClass characterClass;
+  @JsonKey(name: 'element_id')
+  final List<int> elementId;
+  @JsonKey(name: 'critical_ratio')
+  final int criticalRatio;
+  @JsonKey(name: 'critical_damage')
+  final int criticalDamage;
+  // CharacterShotTable
+  @JsonKey(name: 'shot_id')
+  final int shotId;
+  // doesn't know what this means
+  @JsonKey(name: 'bonusrange_min')
+  final int bonusRangeMin;
+  // doesn't know what this means
+  @JsonKey(name: 'bonusrange_max')
+  final int bonusRangeMax;
+  // "AllStep", "Step1", "Step2", "Step3"
+  @JsonKey(name: 'use_burst_skill')
+  final BurstStep useBurstSkill;
+  // "NextStep", "Step1", "Step2", "Step3", "StepFull"
+  @JsonKey(name: 'change_burst_step')
+  final BurstStep changeBurstStep;
+  @JsonKey(name: 'burst_apply_delay')
+  final int burstApplyDelay;
+  // 500, 1000, 1500
+  @JsonKey(name: 'burst_duration')
+  final int burstDuration;
+  // probably default to CharacterSkillTable
+  @JsonKey(name: 'ulti_skill_id')
+  final int ultiSkillId;
+  // CharacterSkillTable
+  @JsonKey(name: 'skill1_id')
+  final int skill1Id;
+  // value is "StateEffect", so use `skill1Id` and search in StateEffectTable
+  // also has value "CharacterSkill", so search in CharacterSkillTable
+  @JsonKey(name: 'skill1_table')
+  final String skill1Table;
+  // CharacterSkillTable
+  @JsonKey(name: 'skill2_id')
+  final int skill2Id;
+  // value is "StateEffect", so use `skill2Id` and search in StateEffectTable
+  // also has value "CharacterSkill", so search in CharacterSkillTable
+  @JsonKey(name: 'skill2_table')
+  final String skill2Table;
+  // doesn't know what this means
+  @JsonKey(name: 'eff_category_type')
+  final String effCategoryType;
+  // doesn't know what this means
+  @JsonKey(name: 'eff_category_value')
+  final int effCategoryValue;
+  // doesn't know what this means
+  @JsonKey(name: 'category_type_1')
+  final String categoryType1;
+  // doesn't know what this means
+  @JsonKey(name: 'category_type_2')
+  final String categoryType2;
+  // doesn't know what this means
+  @JsonKey(name: 'category_type_3')
+  final String categoryType3;
+  final Corporation corporation;
+  @JsonKey(name: 'cv_localkey')
+  final String cvLocalkey;
+  final String squad;
+  // item id for upgrade to next gradeCore, table is ItemPieceTable
+  @JsonKey(name: 'piece_id')
+  final int pieceId;
+  @JsonKey(name: 'is_visible')
+  final bool isVisible;
+  @JsonKey(name: 'prism_is_active')
+  final bool prismIsActive;
+  @JsonKey(name: 'is_detail_close')
+  final bool isDetailClose;
+
+  String get limitBreak => gradeCoreId <= 4 ? 'LimitBreak$gradeCoreId' : 'Core+${gradeCoreId - 4}';
+
+  NikkeCharacterData({
+    this.id = 0,
+    this.nameLocalkey = '',
+    this.descriptionLocalkey = '',
+    this.resourceId = 0,
+    this.additionalSkins = const [],
+    this.nameCode = 0,
+    this.order = 0,
+    this.originalRare = Rarity.unknown,
+    this.gradeCoreId = 0,
+    this.growGrade = 0,
+    this.statEnhanceId = 0,
+    this.characterClass = NikkeClass.unknown,
+    this.elementId = const [],
+    this.criticalRatio = 0,
+    this.criticalDamage = 0,
+    this.shotId = 0,
+    this.bonusRangeMin = 0,
+    this.bonusRangeMax = 0,
+    this.useBurstSkill = BurstStep.unknown,
+    this.changeBurstStep = BurstStep.unknown,
+    this.burstApplyDelay = 0,
+    this.burstDuration = 0,
+    this.ultiSkillId = 0,
+    this.skill1Id = 0,
+    this.skill1Table = '',
+    this.skill2Id = 0,
+    this.skill2Table = '',
+    this.effCategoryType = '',
+    this.effCategoryValue = 0,
+    this.categoryType1 = '',
+    this.categoryType2 = '',
+    this.categoryType3 = '',
+    this.corporation = Corporation.unknown,
+    this.cvLocalkey = '',
+    this.squad = '',
+    this.pieceId = 0,
+    this.isVisible = false,
+    this.prismIsActive = false,
+    this.isDetailClose = false,
+  });
+
+  factory NikkeCharacterData.fromJson(Map<String, dynamic> json) => _$NikkeCharacterDataFromJson(json);
+
+  Map<String, dynamic> toJson() => _$NikkeCharacterDataToJson(this);
+}
+
+@JsonEnum(fieldRename: FieldRename.screamingSnake)
+enum Rarity { unknown, ssr, sr, r }
+
+@JsonEnum(fieldRename: FieldRename.pascal)
+enum NikkeClass { unknown, attacker, defender, supporter }
+
+enum Element {
+  unknown(-1),
+  fire(100001),
+  water(200001),
+  wind(300001),
+  electric(400001),
+  iron(500001);
+
+  final int id;
+
+  const Element(this.id);
+
+  static Element fromId(final int id) {
+    return values.firstWhere(
+      (e) => e.id == id,
+      orElse: () => Element.unknown, // default value if not found
+    );
+  }
+}
+
+@JsonEnum(fieldRename: FieldRename.pascal)
+enum BurstStep {
+  unknown,
+  step1,
+  step2,
+  step3,
+  stepFull,
+
+  // red hood
+  allStep,
+  nextStep,
+}
+
+@JsonEnum(fieldRename: FieldRename.screamingSnake)
+enum Corporation {
+  unknown(-1),
+  missilis(1),
+  elysion(2),
+  tetra(3),
+  pilgrim(4),
+  abnormal(5);
+
+  final int id;
+
+  const Corporation(this.id);
+}
---
+++ lib/model/nikke.g.dart
@@ -0,0 +1,119 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'nikke.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+NikkeCharacterData _$NikkeCharacterDataFromJson(Map<String, dynamic> json) => NikkeCharacterData(
+  id: (json['id'] as num?)?.toInt() ?? 0,
+  nameLocalkey: json['name_localkey'] as String? ?? '',
+  descriptionLocalkey: json['description_localkey'] as String? ?? '',
+  resourceId: (json['resource_id'] as num?)?.toInt() ?? 0,
+  additionalSkins: (json['additional_skins'] as List<dynamic>?)?.map((e) => e as String).toList() ?? const [],
+  nameCode: (json['name_code'] as num?)?.toInt() ?? 0,
+  order: (json['order'] as num?)?.toInt() ?? 0,
+  originalRare: $enumDecodeNullable(_$RarityEnumMap, json['original_rare']) ?? Rarity.unknown,
+  gradeCoreId: (json['grade_core_id'] as num?)?.toInt() ?? 0,
+  growGrade: (json['grow_grade'] as num?)?.toInt() ?? 0,
+  statEnhanceId: (json['stat_enhance_id'] as num?)?.toInt() ?? 0,
+  characterClass: $enumDecodeNullable(_$NikkeClassEnumMap, json['class']) ?? NikkeClass.unknown,
+  elementId: (json['element_id'] as List<dynamic>?)?.map((e) => (e as num).toInt()).toList() ?? const [],
+  criticalRatio: (json['critical_ratio'] as num?)?.toInt() ?? 0,
+  criticalDamage: (json['critical_damage'] as num?)?.toInt() ?? 0,
+  shotId: (json['shot_id'] as num?)?.toInt() ?? 0,
+  bonusRangeMin: (json['bonusrange_min'] as num?)?.toInt() ?? 0,
+  bonusRangeMax: (json['bonusrange_max'] as num?)?.toInt() ?? 0,
+  useBurstSkill: $enumDecodeNullable(_$BurstStepEnumMap, json['use_burst_skill']) ?? BurstStep.unknown,
+  changeBurstStep: $enumDecodeNullable(_$BurstStepEnumMap, json['change_burst_step']) ?? BurstStep.unknown,
+  burstApplyDelay: (json['burst_apply_delay'] as num?)?.toInt() ?? 0,
+  burstDuration: (json['burst_duration'] as num?)?.toInt() ?? 0,
+  ultiSkillId: (json['ulti_skill_id'] as num?)?.toInt() ?? 0,
+  skill1Id: (json['skill1_id'] as num?)?.toInt() ?? 0,
+  skill1Table: json['skill1_table'] as String? ?? '',
+  skill2Id: (json['skill2_id'] as num?)?.toInt() ?? 0,
+  skill2Table: json['skill2_table'] as String? ?? '',
+  effCategoryType: json['eff_category_type'] as String? ?? '',
+  effCategoryValue: (json['eff_category_value'] as num?)?.toInt() ?? 0,
+  categoryType1: json['category_type_1'] as String? ?? '',
+  categoryType2: json['category_type_2'] as String? ?? '',
+  categoryType3: json['category_type_3'] as String? ?? '',
+  corporation: $enumDecodeNullable(_$CorporationEnumMap, json['corporation']) ?? Corporation.unknown,
+  cvLocalkey: json['cv_localkey'] as String? ?? '',
+  squad: json['squad'] as String? ?? '',
+  pieceId: (json['piece_id'] as num?)?.toInt() ?? 0,
+  isVisible: json['is_visible'] as bool? ?? false,
+  prismIsActive: json['prism_is_active'] as bool? ?? false,
+  isDetailClose: json['is_detail_close'] as bool? ?? false,
+);
+
+Map<String, dynamic> _$NikkeCharacterDataToJson(NikkeCharacterData instance) => <String, dynamic>{
+  'id': instance.id,
+  'name_localkey': instance.nameLocalkey,
+  'description_localkey': instance.descriptionLocalkey,
+  'resource_id': instance.resourceId,
+  'additional_skins': instance.additionalSkins,
+  'name_code': instance.nameCode,
+  'order': instance.order,
+  'original_rare': _$RarityEnumMap[instance.originalRare]!,
+  'grade_core_id': instance.gradeCoreId,
+  'grow_grade': instance.growGrade,
+  'stat_enhance_id': instance.statEnhanceId,
+  'class': _$NikkeClassEnumMap[instance.characterClass]!,
+  'element_id': instance.elementId,
+  'critical_ratio': instance.criticalRatio,
+  'critical_damage': instance.criticalDamage,
+  'shot_id': instance.shotId,
+  'bonusrange_min': instance.bonusRangeMin,
+  'bonusrange_max': instance.bonusRangeMax,
+  'use_burst_skill': _$BurstStepEnumMap[instance.useBurstSkill]!,
+  'change_burst_step': _$BurstStepEnumMap[instance.changeBurstStep]!,
+  'burst_apply_delay': instance.burstApplyDelay,
+  'burst_duration': instance.burstDuration,
+  'ulti_skill_id': instance.ultiSkillId,
+  'skill1_id': instance.skill1Id,
+  'skill1_table': instance.skill1Table,
+  'skill2_id': instance.skill2Id,
+  'skill2_table': instance.skill2Table,
+  'eff_category_type': instance.effCategoryType,
+  'eff_category_value': instance.effCategoryValue,
+  'category_type_1': instance.categoryType1,
+  'category_type_2': instance.categoryType2,
+  'category_type_3': instance.categoryType3,
+  'corporation': _$CorporationEnumMap[instance.corporation]!,
+  'cv_localkey': instance.cvLocalkey,
+  'squad': instance.squad,
+  'piece_id': instance.pieceId,
+  'is_visible': instance.isVisible,
+  'prism_is_active': instance.prismIsActive,
+  'is_detail_close': instance.isDetailClose,
+};
+
+const _$RarityEnumMap = {Rarity.unknown: 'UNKNOWN', Rarity.ssr: 'SSR', Rarity.sr: 'SR', Rarity.r: 'R'};
+
+const _$NikkeClassEnumMap = {
+  NikkeClass.unknown: 'Unknown',
+  NikkeClass.attacker: 'Attacker',
+  NikkeClass.defender: 'Defender',
+  NikkeClass.supporter: 'Supporter',
+};
+
+const _$BurstStepEnumMap = {
+  BurstStep.unknown: 'Unknown',
+  BurstStep.step1: 'Step1',
+  BurstStep.step2: 'Step2',
+  BurstStep.step3: 'Step3',
+  BurstStep.stepFull: 'StepFull',
+  BurstStep.allStep: 'AllStep',
+  BurstStep.nextStep: 'NextStep',
+};
+
+const _$CorporationEnumMap = {
+  Corporation.unknown: 'UNKNOWN',
+  Corporation.missilis: 'MISSILIS',
+  Corporation.elysion: 'ELYSION',
+  Corporation.tetra: 'TETRA',
+  Corporation.pilgrim: 'PILGRIM',
+  Corporation.abnormal: 'ABNORMAL',
+};
